6a5257d8a9a5161e507741c7fc16e48668ab12fe
mathur15 <som.mathur1997@gmail.com>
2019-09-21 16:41:50 -0400

    Using strcpy and strncpy

--- string_manipulation.c
+++ string_manipulation.c
@@ -13,6 +13,28 @@ int main(){
 	printf("The length of the string is %lu\n", strlen(text3));
 	printf("%s",text3);
 
+	//copying string using strcpy
+	char cpy1[20];
+	char cpy2[10]= "Monday";
+
+	strcpy(cpy1,cpy2);
+	printf("Using strcpy %s \n",cpy1);
+	printf("Using strcpy %s\n", cpy2);
+
+	//copying using strncpy
+
+	char strncpy1[10];
+	char strncpy2[20]= "I love university of Toronto";
+
+	strncpy(strncpy1,strncpy2,sizeof(strncpy1)); 
+	/* At this point since null terminator will not be found in first 10 chars of strncpy2, null terminator wont be added */
+
+	strncpy[9]="\0";
+
+	printf("Using strncpy %s\n", strncpy1);
+	printf("Using strncpy %s\n", strncpy2);
+
+
 
 
 }
